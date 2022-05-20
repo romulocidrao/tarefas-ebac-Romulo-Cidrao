5f4857445eac5887bed17790952b047c67861a1e
romulocidrao <romulo_cidrao@msn.com>
2022-05-19 21:02:09 -0300

    Versionamento do Código referente à atividade do módulo 30

---
+++ mod30/Projeto-3-Parte-2/Projeto3-Parte2/src/main/java/anotacao/TipoChave.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package anotacao;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Rômulo
+ *
+ */
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TipoChave {
+
+    String value();
+}
---
+++ mod30/Projeto-3-Parte-2/Projeto3-Parte2/src/main/java/br/com/rcidrao/services/IProdutoService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package br.com.rcidrao.services;
+
+import br.com.rcidrao.domain.Produto;
+import br.com.rcidrao.services.generic.IGenericService;
+
+/**
+ * @author Rômulo
+ *
+ */
+public interface IProdutoService extends IGenericService<Produto, String> {
+
+}
